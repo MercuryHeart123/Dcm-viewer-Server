061abadaba27d6a5d10ec59cee6aa664243d0eb7
MercuryHeart123 <heartkung35@gmail.com>
2021-10-10 18:35:09 +0700

    list function have new parameter:index

--- serverapi.js
+++ serverapi.js
@@ -4,42 +4,70 @@ const basename = require("path").basename
 const express = require("express")
 const app = express()
 const fs = require('fs');
+const LocalFolder = './dcm/local/';
 const UploadFolder = './dcm/upload/';
-var uploadFilelist = [];
+
 const cors = require("cors");
 const corsOptions ={ origin:'*', credentials:true, }
-
+var lenLocal ;
+var lenUpload;
 
 async function* tokenise (path = ".")
 { yield { dir: path }
+  
   for (const dirent of await readdir(path, { withFileTypes: true }))
     if (dirent.isDirectory())
       yield* tokenise(join(path, dirent.name))
     else
       yield { file: join(path, dirent.name) }
+
   yield { endDir: path }
 }
 
-async function parse (iter = empty())
+async function parse (iter = empty(), index, islocal=true)
 { const r = [{}]
+  var a=0;
+  var fileIndex = 0;
+  var end =false;
   for await (const e of iter)
-    if (e.dir)
+    if (e.dir){
       r.unshift({})
-    else if (e.file)
+
+    }
+    else if (e.file){
+      if (fileIndex >= index && islocal == false){ // skip index that unwanted 
+        continue;
+      }
+      end = true;
+      fileIndex++;
       r[0][basename(e.file)] = true
-    else if (e.endDir)
+      
+    }
+
+    else if (e.endDir){
+      if(end == true){
+        a++
+        end = false;
+      }
       r[1][basename(e.endDir)] = r.shift()
+      if(Object.keys(r[0]).length == index && islocal){
+        return r[0]
+      }
+      
+
+      }
+
   return r[0]
 }
 
 async function* empty () {}
 
-
+fs.readdir(LocalFolder, (err, files) => {
+  lenLocal = files.length;
+})
 
 fs.readdir(UploadFolder, (err, files) => {
-  files.forEach(file => {
-    uploadFilelist.push(file);
-  });
+  lenUpload = files.length;
 })
 
 async function start(){
@@ -48,37 +76,46 @@ async function start(){
   app.use(express.urlencoded({limit: '50mb'}));
 
   app.post('/upload', (req,res) => {
-    var len = String(uploadFilelist.length);
+    var len = lenUpload.toString();;
     let base64 = req.body.file;
     let base64Image = base64.split(';base64,').pop();
+
     while(len.length<=6){
       len = '0'+len
+
     }
 
     fs.writeFile(`./dcm/upload/${len}.dcm`, base64Image, {encoding: 'base64'}, function(err) {
       console.log(`${len}.dcm created `);
     });
-    uploadFilelist.push(`${len}.dcm`)
+    lenUpload+=1;
   })
 
 
-  app.get('/list/local', (req,res) => {
+  app.get('/list/local/:index', (req,res) => {
+    var index = req.params.index;
+    const createTree = (path = ".", index) =>
+        parse(tokenise(path), index)
 
-    const createTree = (path = ".") =>
-        parse(tokenise(path))
-
-    createTree("./dcm/local/")
-      .then(r => res.send(JSON.stringify(r, null, 2)))
+    createTree("./dcm/local/", index)
+    .then(r => {
+                  r[`MaxIndex`] = lenLocal;
+                  res.send(JSON.stringify(r, null, 2))
+                })
       .catch(console.error)
     console.log("Local list");
   })
 
-  app.get('/list/upload', (req,res) => {
-    const createTree = (path = ".") =>
-        parse(tokenise(path))
+  app.get('/list/upload/:index', (req,res) => {
+    var index = req.params.index;
+    const createTree = (path = ".", index) =>
+        parse(tokenise(path), index, false)
 
-    createTree("./dcm/upload/")
-      .then(r => res.send(JSON.stringify(r, null, 2)))
+    createTree("./dcm/upload/", index)
+      .then(r => {
+                    r[`MaxIndex`] = lenUpload;
+                    res.send(JSON.stringify(r, null, 2))
+                  })
       .catch(console.error)
     console.log("Upload listed");
   })
@@ -86,6 +123,11 @@ async function start(){
   app.get('/dcm/*', function(req, res){
 
     var id = req.originalUrl;
+    if (!id.includes('local') && !id.includes('upload')){
+      id = id.replace('/dcm','/dcm/local')  // url that pass this path sometime miss local directory 
+                                            // this is filter before load img 
+    }
+    console.log(id);
     id = decodeURI(id)
     try{
         const file = `.${id}`;
